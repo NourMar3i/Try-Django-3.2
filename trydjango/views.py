409d220cb04326ff8340c30cbfadd02af379c4f4
NourMar3i <80076377+NourMar3i@users.noreply.github.com>
2023-06-08 12:16:34 +0400

    Update views.py

--- trydjango/views.py
+++ trydjango/views.py
@@ -1,26 +1,31 @@
 """
-render html
+To render html web pages
 """
-from django.shortcuts import render
+import random
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from articles.models import Article 
-import random
-def home_view(request,*args,**kwargs):
-    print(id)
-    rand_id=random.randint(1,4)
-    article_obj=Article.objects.get(id=rand_id)
+from articles.models import Article
+
+def home_view(request, *args, **kwargs):
+    """
+    Take in a request (Django sends request)
+    Return HTML as a response (We pick to return the response)
+    """
+    name = "Justin" # hard coded
+    random_id = random.randint(1, 4) # pseudo random
+    
+    # from the database??
+    article_obj = Article.objects.all().first()
     article_queryset = Article.objects.all()
-
     context = {
-        "object_list":article_queryset,
+        "object_list": article_queryset,
         "object": article_obj,
-        "title": article_obj.title,
-        "id": article_obj.id,
-        "content": article_obj.content,
-
     }
-    HTML_STRING = render_to_string("home-view.html",context = context)
-
+    # Django Templates
+    HTML_STRING = render_to_string("home-view.html", context=context)
+    # HTML_STRING = """
+    # <h1>{title} (id: {id})!</h1>
+    # <p>{content}!</p>
+    # """.format(**context)
     #return HttpResponse(HTML_STRING)
     return render(request, "home-view.html", context=context)
